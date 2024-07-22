5faaf960e5f9628a0feaeae192b61e74dd34057c
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2024-07-22 18:00:48 +0700

    feat: implement create all component icons

---
+++ training-react-advance/purity-dashboard/src/lib/ui/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { Box, Flex } from "@chakra-ui/react";
+
+// Component
+import { LogoIcon } from "@/icons";
+
+
+const Header = () => (
+  <Flex>
+    <Box>
+      <LogoIcon />
+    </Box>
+  </Flex>
+)
