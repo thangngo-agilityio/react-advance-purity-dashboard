e35477644b8421b716fb11f4045f3545100ce42f
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2024-07-29 09:42:18 +0700

    feat: implement query provider and chakra provider

---
+++ training-react-advance/purity-dashboard/src/providers/QueryProvider/index.tsx
@@ -0,0 +1,26 @@
+import { memo, ReactNode } from 'react';
+import { QueryClient } from '@tanstack/react-query';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+
+
+export const queryClient = new QueryClient({
+  defaultOptions: {},
+});
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
+const QueryProvider = ({ children }: { children?: ReactNode }) => (
+  <PersistQueryClientProvider
+    client={queryClient}
+    persistOptions={{
+      persister,
+    }}
+  >
+    {children}
+  </PersistQueryClientProvider>
+);
+
+export default memo(QueryProvider);
