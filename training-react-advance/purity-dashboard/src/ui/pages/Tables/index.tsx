228c83a89e323501a0a45908686a25518837844f
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2024-08-07 11:13:51 +0700

    feat: create headcell

--- training-react-advance/purity-dashboard/src/ui/pages/Tables/index.tsx
+++ training-react-advance/purity-dashboard/src/ui/pages/Tables/index.tsx
@@ -1,22 +1,38 @@
-import { useMemo } from 'react';
-import { VStack } from '@chakra-ui/react';
+import { useCallback, useMemo } from 'react';
+import { Th, VStack } from '@chakra-ui/react';
 
 // Components
 import Header from '@/ui/components/Header';
 import ModalTable from '@/ui/components/ModalTable';
+import HeadCell from '@/ui/components/Table/HeadCell';
 
 // Constants
 import { COLUMNS_AUTHOR, COLUMNS_PROJECT, ROUTES } from '@/lib/constants';
 
 const TablePage = () => {
+  const isAuth: boolean = true
 
+  const renderHead = useCallback(
+    (title: string, key: string): JSX.Element => {
+
+      return title ? (
+        <HeadCell key={key} title={title} />
+      ) : (
+        <Th w={50} maxW={50} />
+      );
+    }, [],
+  );
+
+  const columns = useMemo(() => {
+    isAuth ? COLUMNS_AUTHOR(renderHead) : COLUMNS_PROJECT(renderHead)
+  }, [])
 
   return (
     <VStack alignItems='flex-start'>
       <Header name="Tables" path={ROUTES.TABLES} />
 
       <VStack gap='24px' w='100%'>
-        <ModalTable title='Authors Table' />
+        <ModalTable title='Authors Table' columns={columns} />
         <ModalTable title='Projects' />
       </VStack>
     </VStack>
