597c858f6f67361b963fefaafbe9797fd9e86a01
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2024-08-15 09:23:45 +0700

    feat: update ui for profile and footer

--- training-react-advance/purity-dashboard/src/ui/components/Footer/index.tsx
+++ training-react-advance/purity-dashboard/src/ui/components/Footer/index.tsx
@@ -7,18 +7,18 @@ export type TFooterProps = {
 }
 
 const Footer = ({ isAuth }: TFooterProps) => (
-  <Stack w="100%" flexDirection={{ base: 'column', lg: 'row' }} px={isAuth ? '30px' : ''} pt={isAuth ? '4px' : ''} pb={isAuth ? '20px' : ''} alignItems={{ base: "center", lg: 'flex-end' }} justifyContent={{ base: "center", lg: "space-between"}}>
+  <Stack w="100%" flexDirection={{ base: 'column', lg: 'row' }} px={isAuth ? '30px' : ''} pt={isAuth ? '4px' : ''} pb={isAuth ? '20px' : ''} alignItems={{ base: "center", lg: 'flex-end' }} justifyContent={{ base: "center", lg: "space-between" }}>
     <HStack gap="2px">
-      <Text content="@ 2021, Made with ❤️ by" size="textSm" />
+      <Text content="@ 2021, Made with ❤️ by" size={{ base: "textXs", lg: "textSm" }} />
       <Text
         content="Creative Tim"
         color="text.300"
         fontWeight="700"
-        size="textSm"
+        size={{ base: "textXs", lg: "textSm" }}
       />
-      <Text content="&" size="textSm" />
-      <Text content="Simple" color="text.300" fontWeight="700" size="textSm" />
-      <Text content="for a better web" size="textSm" />
+      <Text content="&" size={{ base: "textXs", lg: "textSm" }} />
+      <Text content="Simple" color="text.300" fontWeight="700" size={{ base: "textXs", lg: "textSm" }} />
+      <Text content="for a better web" size={{ base: "textXs", lg: "textSm" }} />
     </HStack>
 
     <List
