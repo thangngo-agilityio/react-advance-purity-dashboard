dde80faa104e462fbf933696d6e15f677734c88c
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2024-08-27 10:06:50 +0700

    fix: update table cell component

--- training-react-advance/purity-dashboard/src/ui/components/Table/Body/FunctionCell.tsx
+++ training-react-advance/purity-dashboard/src/ui/components/Table/Body/FunctionCell.tsx
@@ -7,18 +7,14 @@ const FunctionCell = ({ job, role }: TDataSource) => (
   <Td px={0} w={{ base: '200px', xl: '220px', '3xl': '250px', '6xl': '350px' }}>
     <VStack gap={0} alignItems="flex-start" pr="20px">
       <Heading
-        size="md"
-        textOverflow="ellipsis"
-        overflow="hidden"
         w={{ base: '200px', xl: '220px', '3xl': '250px', '6xl': '350px' }}
+        size="md"
       >
         {role as string}
       </Heading>
       <Text
-        variant="tertiary"
-        textOverflow="ellipsis"
-        overflow="hidden"
         w={{ base: '200px', xl: '220px', '3xl': '250px', '6xl': '350px' }}
+        variant="tertiary"
       >
         {job as string}
       </Text>
